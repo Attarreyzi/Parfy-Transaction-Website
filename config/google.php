e3bbe0133c5c80db620ec0f2227a89c334421265
Attarreyzi <reyzi.put@gmail.com>
2026-08-21 00:36:02 +0700

    feat: pasang Google OAuth Client ID resmi pada config/google.php

--- config/google.php
+++ config/google.php
@@ -1,6 +1,6 @@
 <?php
 /**
  * Konfigurasi Google OAuth Client ID
- * Dapatkan Client ID dari Google Cloud Console: https://console.cloud.google.com
  */
-define('GOOGLE_CLIENT_ID', getenv('GOOGLE_CLIENT_ID') ?: 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_ID', '650909013258-qe27oj4v555gs22p08d3s4p4kgh09m4o.apps.googleusercontent.com');
+
